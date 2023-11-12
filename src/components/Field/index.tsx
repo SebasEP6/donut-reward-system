dc3da1cd7b8d2bf597d90844011b1dbe17e5b59d
Sebastian Escalona <sebastianep6@gmail.com>
2023-11-12 11:57:34 -0300

    updated field component

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -8,12 +8,18 @@ const Field = (props) => {
     type = "text"
   } = props;
 
-  const formatName = (name: string): string => name.trim().replace(/\s/g,"_").toLowerCase();
+  const getCustomID = () => Math.floor(Math.random() * 100);
+
+  const formatName = (): string => 
+    name.trim().replace(/\s/g,"_").toLowerCase()
+  ;
+
+  const inputName = `${formatName()}_${getCustomID()}`
 
   return (
     <StyledField>
       <StyledLabel>{name}</StyledLabel>
-      <StyledInput type={type} value={value} onChange={e => setValue(e.target.value.trim())} name={formatName(name)} />
+      <StyledInput type={type} value={value} onChange={e => setValue(e.target.value.trim())} name={inputName} />
     </StyledField>
   );
 };
